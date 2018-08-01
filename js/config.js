1e2eebbca6381d9dac4bbdf815f328f2a0750c59
Nex <nex@nex.sx>
2018-08-01 20:32:26 +0200

    Changing pre-configured backend

--- js/config.js
+++ js/config.js
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with PhishDetect.  If not, see <https://www.gnu.org/licenses/>.
 
-const BACKEND_DEFAULT_DOMAIN = "https://phishdetect.io";
+const BACKEND_DEFAULT_DOMAIN = "https://node.phishdetect.io";
 
 function setBackendDomain(domain) {
     localStorage.setItem("server", domain);

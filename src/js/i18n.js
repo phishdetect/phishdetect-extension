02667268dd81236e93e93c4352ab70c73be95d6c
Nex <nex@nex.sx>
2019-10-30 12:51:42 +0100

    Fixed indentation

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -1,6 +1,23 @@
-$('[data-resource]').each(function() {
-  var el = $(this);
-  var resourceName = el.data('resource');
-  var resourceText = chrome.i18n.getMessage(resourceName);
-  el.text(resourceText);
+// Copyright (c) 2018-2019 Claudio Guarnieri.
+//
+// This file is part of PhishDetect.
+//
+// PhishDetect is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PhishDetect is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PhishDetect.  If not, see <https://www.gnu.org/licenses/>.
+
+$("[data-resource]").each(function() {
+    var el = $(this);
+    var resourceName = el.data("resource");
+    var resourceText = chrome.i18n.getMessage(resourceName);
+    el.text(resourceText);
 });

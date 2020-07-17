f3a4826707454812697a53a3edbf19f4ea480194
Nex <nex@nex.sx>
2020-07-17 13:14:13 +0200

    Refactoring styles

--- src/components/History.js
+++ src/components/History.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export function HistoryAlert(props) {
     return (
-        <div>{props.dateTime} suspicious visit to {props.url}</div>
+        <div className="p-2 mb-2 border-l-4 border-gray-300">{props.dateTime} suspicious visit to <b>{props.url}</b></div>
     );
 }

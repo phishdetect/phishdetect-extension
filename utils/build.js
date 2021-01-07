776ad43c77a7e15ab5258b0ca50cb23486a29a63
Donncha Ó Cearbhaill <donncha@donncha.is>
2021-01-07 12:23:16 +0100

    Remove unused build.js file

--- utils/build.js
+++
@@ -1,9 +0,0 @@
-const webpack = require("webpack");
-var config = require("../webpack.config");
-
-delete config.chromeExtensionBoilerplate;
-
-webpack(
-    config,
-    function (err) { if (err) throw err; }
-);

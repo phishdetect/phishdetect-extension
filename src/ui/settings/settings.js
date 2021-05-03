a90dbefb3d40ec7fb52b1c2d057aba0e1e756357
Nex <nex@nex.sx>
2021-05-03 13:46:28 +0200

    Actually toggle settings

--- src/ui/settings/settings.js
+++ src/ui/settings/settings.js
@@ -17,15 +17,16 @@
 
 function loadOptions() {
     $("#server").val(cfg.getNode());
-    $("#webmails").prop("checked", cfg.getWebmailsIntegration());
+    $("#toggleIntegrations").prop("checked", cfg.getWebmailsIntegration());
 
     if (cfg.status == "offline") {
     } else if (cfg.status == "unauthorized") {
     }
 
     const sendAlerts = cfg.getSendAlerts();
-    $("#sendAlerts").prop("checked", cfg.getSendAlerts());
+    $("#toggleReporting").prop("checked", cfg.getSendAlerts());
     $("#contact").val(cfg.getUserContact());
+
     if (sendAlerts) {
         $("#contactLabel").removeClass("text-gray-500");
         $("#contact").prop("disabled", false);
@@ -40,50 +41,28 @@ function loadOptions() {
     }
 }
 
-function saveOptions(event) {
-    event.preventDefault();
-    const node = $("#server").val().trim();
-    var key = $("#key").val().trim();
-    if (node != "" && cfg.getNode() != node) {
-        // Set request to background to update node and reset config.
-        const container = $("#container").empty();
-        chrome.runtime.sendMessage({method: "updateNode", node: node, key: key}, function(response) {
-            // Saved
-        });
-    } else {
-        cfg.setApiKey(key);
-        cfg.setContact($("#contact").val().trim());
-        cfg.setSentAlerts($("#sendAlerts").is(":checked"));
-        cfg.setWebmailsIntegration($("#webmails").is(":checked"));
+function saveOptions() {
+    // cfg.setContact($("#contact").val().trim());
+    cfg.setWebmailsIntegration($("#toggleIntegrations").is(":checked"));
+    cfg.setSentAlerts($("#toggleReporting").is(":checked"));
 
-        // Reload config on background page
-        const container = $("#container").empty();
-        chrome.runtime.sendMessage({method: "updateConfiguration", config: cfg.config}, function(response) {
-            // Saved
-            // Update indicators in background script after we have potential edited API key.
-            chrome.runtime.sendMessage({method: "updateIndicators"});
-        });
-    }
-}
-
-function restoreDefaults() {
-    $("#server").val(NODE_DEFAULT_URL);
-    $("#key").val("");
-    $("#webmails").prop("checked", true);
-    $("#sendAlerts").prop("checked", true);
-    $("#contact").val("");
+    // Reload config on background page
+    chrome.runtime.sendMessage({method: "updateConfiguration", config: cfg.config});
 }
 
 document.addEventListener("DOMContentLoaded", cfg.loadFromBackground(loadOptions));
-$("#saveOptions").click(saveOptions);
-$("#restoreDefaults").click(restoreDefaults);
 
-$("#sendAlerts").change(function() {
-    if (this.checked) {
-        $("#contactLabel").removeClass("text-gray-500");
-        $("#contact").prop("disabled", false);
-    } else {
-        $("#contactLabel").addClass("text-gray-500");
-        $("#contact").prop("disabled", true);
-    }
+$("#toggleIntegrations").change(function() {
+    saveOptions();
+});
+
+$("#toggleReporting").change(function() {
+    saveOptions();
+    // if (this.checked) {
+    //     $("#contactLabel").removeClass("text-gray-500");
+    //     $("#contact").prop("disabled", false);
+    // } else {
+    //     $("#contactLabel").addClass("text-gray-500");
+    //     $("#contact").prop("disabled", true);
+    // }
 });

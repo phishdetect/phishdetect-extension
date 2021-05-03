d4f4a44b8f84ba8b9dfba9c38f39ed2aa5bbdc12
Nex <nex@nex.sx>
2021-05-03 15:57:22 +0200

    Added process to leave the existing network

---
+++ src/ui/settings/leave_confirm.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2018-2021 Claudio Guarnieri.
+//
+// This file is part of PhishDetect.
+//
+// PhishDetect is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PhishDetect is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PhishDetect.  If not, see <https://www.gnu.org/licenses/>.
+
+document.addEventListener("DOMContentLoaded", cfg.loadFromBackground(function() {
+    $("#networkAddress").text(cfg.getNode());
+    chrome.runtime.sendMessage({method: "updateNode", node: NODE_DEFAULT_URL});
+}));
